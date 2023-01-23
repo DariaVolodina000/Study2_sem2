46befe83bf091c6fe8aa99a67d7fae6a6718e94b
Daria <dashavolodina@gmail.com>
2023-01-23 09:25:22 +0800

    Добавлена фунцкия приглашения ко вводу

--- sem2_001/Program.cs
+++ sem2_001/Program.cs
@@ -3,12 +3,24 @@
 // 782 -> 2
 // 918 -> 8
 
-    Console.WriteLine("Введите трехзначное число >");
-int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+// Console.WriteLine("Введите трехзначное число >");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+int IntputInt (string massage) // функция, которая пишет сообщение, и принимает число, по сути то же что и выше
+{
+    Console.WriteLine(massage);
+    int a = Convert.ToInt32(Console.ReadLine());
+    return a;
+}
+
+int num = IntputInt ("Введите трехзначное число");
 if (num > 99 & num < 1000)
 {
     int result = num % 10;
-    Console.WriteLine($"Последняя цифра числа равна  " +  result);
+    Console.WriteLine($"Последняя цифра числа равна " + result);
 }
 else
 {

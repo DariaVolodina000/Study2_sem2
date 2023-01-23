648f920aa1c90760745cac3c811fa9ef5d43f0f2
Daria <dashavolodina@gmail.com>
2023-01-23 11:30:05 +0800

    а кратное b или нет

---
+++ sem2_003/Program.cs
@@ -0,0 +1,23 @@
+﻿// 3. Напишите программу, которая будет принимать 
+// на вход два числа и выводить, является ли 
+// второе число кратным первому. Если число 2 
+// не кратно числу 1, то программа выводит 
+// остаток от деление.
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+int IntputInt(string message)
+{
+    Console.WriteLine(message);
+    int x = Convert.ToInt32(Console.ReadLine());
+    return x;
+}
+
+int a = IntputInt("Введите делимое число");
+int b = IntputInt("Введите делитель");
+while (a >= b)
+{
+    a = a - b;
+}
+if (a == 0) Console.WriteLine("кратно");
+else Console.WriteLine("не кратно, остаток " +a );
